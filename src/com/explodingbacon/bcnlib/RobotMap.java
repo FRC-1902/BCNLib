9a85c5e64b59466226c4fdc4e9d928aa2febb92d
Dominic Canora <dominiccanora@gmail.com>
2016-01-04 18:19:12 -0500

    Command and Subsystem are minimally working. Created empty files for OI and RobotMap

---
+++ src/com/explodingbacon/bcnlib/RobotMap.java
@@ -0,0 +1,5 @@
+package com.explodingbacon.bcnlib;
+
+public class RobotMap {
+
+}
---
+++ src/com/explodingbacon/bcnlib/Subsystem.java
@@ -0,0 +1,55 @@
+package com.explodingbacon.bcnlib;
+
+/**
+ * @author Dominic Canora
+ * @version 2016.1.0
+ */
+public abstract class Subsystem {
+    /**
+     * <code>Command</code> object that is holding control of this <code>Subsystem</code>. <code>Null</code> when this
+     * <code>Subsystem</code> is able to be changed
+     */
+    Command inUseBy;
+
+    /**
+     * Allows a <code>Command</code> to take exclusive control of a <code>Subsystem</code>.
+     *
+     * @param c Subclass of <code>Command</code> that is taking exclusive control of this <code>Subsystem</code>
+     */
+    public void takeControl(Command c) {
+        if (this.inUseBy == null)
+            this.inUseBy = c;
+        else
+            throw new RuntimeException("Command attempted to take control of a subsystem that was already being controlled");
+    }
+
+    /**
+     * Allows a <code>Command</code> to release control of a <code>Subsystem</code>.
+     *
+     * @param c Subclass of <code>Command</code> that is releasing control of this <code>Subsystem</code>
+     */
+    public void releaseControl(Command c) {
+        if (c == inUseBy) {
+            inUseBy = null;
+        } else {
+            throw new RuntimeException("Command attempted to release a Subsystem it was not in control of");
+        }
+    }
+
+    /**
+     * Gets whether this <code>Subsystem</code> is in use.
+     *
+     * @return True if this <code>Subsystem</code> is in use
+     */
+    public boolean isInUse() {
+        return inUseBy != null;
+    }
+
+    /**
+     * Void that should stop all motion in this <code>Subsystem</code> when called.
+     * <p>
+     * Called automatically when a <code>Command</code> takes control of this <code>Subsystem</code>, and can be called
+     * by any <code>Command</code> using any <code>Subsystem</code> for ease of control.
+     */
+    public abstract void stop();
+}
