23bf6e5851dd922d7c37afb8eaa2039a404b54aa
Dominic Canora <Dominic Canora>
2016-01-05 10:44:59 -0500

    Rename package folders

---
+++ src/com/explodingbacon/bcnlib/actuators/Victor.java
@@ -0,0 +1,8 @@
+package com.explodingbacon.bcnlib.Actuators;
+
+public class Victor extends edu.wpi.first.wpilibj.Victor implements com.explodingbacon.bcnlib.actuators.Motor{
+
+    public Victor(int channel) {
+        super(channel);
+    }
+}
