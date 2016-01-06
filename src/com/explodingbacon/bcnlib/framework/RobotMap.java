c503985f86832ca51c0556b69c9348223d054421
Dominic <dominiccanora@gmail.com>
2016-01-06 10:48:43 -0500

    More Javadoc Comments

--- src/com/explodingbacon/bcnlib/framework/RobotMap.java
+++ src/com/explodingbacon/bcnlib/framework/RobotMap.java
@@ -11,10 +11,20 @@ import java.util.Map;
 public class RobotMap {
     private Map<String, Integer> m =  new HashMap<>();
 
+    /**
+     * Register a device/port combo.
+     * @param id The ID of the device you'd like to refer to it by
+     * @param port The port that the device is in
+     */
     private void register(String id, Integer port) {
         m.put(id, port);
     }
 
+    /**
+     * Retrieve the port of a device
+     * @param id The ID of the device that you'd like to refer to
+     * @return The port that the device is in
+     */
     public Integer get(String id) {
         return m.get(id);
     }
